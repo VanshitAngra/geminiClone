d29200419f8eb761542d461c4e19bf25a2bcbb31
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-07-18 02:53:08 +0530

    addition of file .env and context

---
+++ src/context/Context.jsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+export const Context=createContext();
+const ContextProvider=(props)=>{
+
+    const contextValue={
+
+    }
+    return(
+        <Context.Provider value={contextValue}>
+
+        </Context.Provider>
+    )
+} 
+export default ContextProvider
