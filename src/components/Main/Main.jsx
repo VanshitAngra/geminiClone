222663e23a9d1176664764e26e7341dc92e21fff
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-07-18 02:24:32 +0530

    completion of UI of project

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -31,6 +31,19 @@ const Main = () => {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a prompt here'/>
+                    <div className='image'>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">
+                Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
+
+            </div>
         </div>
       
     </div>
