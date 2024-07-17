f03eb892e2e12f5f11195d7aa807445cf00e42b5
VanshitAngra <vanshit1519.be21@chitkara.edu.in>
2024-07-17 17:55:57 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
+import Sidebar from '../components/Sidebar/Sidebar'
 
 const App = () => {
   return (
+    <>
     <Sidebar/>
+    </>
+    
   )
 }
 
